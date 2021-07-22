29403ba4074109c7907a5f57e1171cddf5434dd8
Jklein64 <2022jklein@tjhsst.edu>
2021-07-22 00:05:54 -0400

    Remove comment

--- renderer/src/input.ts
+++ renderer/src/input.ts
@@ -210,8 +210,6 @@ function handleSelection(vertices: THREE.Vector2[]) {
                     ({ rgb: { r, g, b }, xy: { x, y } }) => `${r} ${g} ${b} ${x} ${y}`))
                 state.selectedList = state.selectedList.filter(
                     ({ rgb: { r, g, b }, xy: { x, y } }) => !selectedSet.has(`${r} ${g} ${b} ${x} ${y}`))
-                // state.selectedList = state.selectedList.filter(inV =>
-                //     selected.map(v => v[1]).every(outV => !outV.equals(inV)))
                 break
         }
 
